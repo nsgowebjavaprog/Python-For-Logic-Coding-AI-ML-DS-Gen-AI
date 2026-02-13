44e518e0c9b01db0591b611c7c5e1e6b75353e81
ns loni <nsloni@gmail.com>
2026-02-13 10:31:15 +0530

    Yes

---
+++ Day-1/Start.py
@@ -0,0 +1,30 @@
+#---------------------------variables---------------------------------
+# variables:- Store the value.
+
+# String
+first_name = "Bro"
+food = "pizza"
+email = "Bro123@fake.com"
+
+# Integer
+age = 25
+quantity = 3
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+
+# float
+price = 10.99
+cgpa = 9.50
+print(f"The price is ${price}.")
+print(f"Your cgpa is: {cgpa}")
+
+# boolean
+is_student = True
+is_graduate = False
+
+print(f"Are you a Student: {is_student}.")
+print(f"Are you a Graduate: {is_graduate}.")
+
+#-----------------------------Type-Casting-------------------------------
+
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Python-For-Logic-Coding-AI-ML-DS-Gen-AI
 Python for all what ever is available in python like AI, ML, Data Science, Generative AI, Logics, MLOPs, Agentic AI, DSA and many more....
+
+## Day-1
+## -------------------------------------------------------------
+## 1   python tutorial for beginners 🐍
+## 2   variables ❎
+## 3   type casting 💱
+## 4   user input ⌨️
+## 5  ⭐ madlibs game 📖
+## 6   arithmetic & math 📐
+## 7   if statements 🤔
+## 8  ⭐ calculator program 🧮
+## 9  ⭐ weight conversion program 🏋️
+## 10 ⭐ temperature conversion program 🌡️
+
+## -------------------------------------------------------------
